b4bb36e069d33df1655c75473b5f812225336349
EHoldener <eholdener12@gmail.com>
2022-05-07 16:44:06 -0700

    finished section html form of forms project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,27 +8,19 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create
+    # @user = User.new(username: params[:user], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
